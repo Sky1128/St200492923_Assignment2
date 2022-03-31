72326ca21bd899f6d8c1dc142cd5f13071e91525
sky1128 <pa.pruthvish@gmail.com>
2022-03-30 23:26:25 -0400

    Initial commit

---
+++ src/main/java/com/example/st200492923_assignment2/Heroes.java
@@ -0,0 +1,76 @@
+package com.example.st200492923_assignment2;
+
+import javafx.scene.image.Image;
+
+public class Heroes {
+
+    private String heroName;
+    private String heroType;
+    private String heroRole;
+    private double attackDamage;
+    private double armor;
+    private String abilities;
+    private Image heroImage;
+
+    public Heroes(String heroName, String heroType, String heroRole, double attackDamage, double armor, String abilities)
+    {
+        setHeroName(heroName);
+        setHeroType(heroType);
+        setHeroRole(heroRole);
+        setAttackDamage(attackDamage);
+        setArmor(armor);
+        setAbilities(abilities);
+    }
+
+    public String getHeroName() {
+        return heroName;
+    }
+
+    public void setHeroName(String heroName) {
+        this.heroName = heroName;
+    }
+
+    public String getHeroType() {
+        return heroType;
+    }
+
+    public void setHeroType(String heroType) {
+        this.heroType = heroType;
+    }
+
+    public String getHeroRole() {
+        return heroRole;
+    }
+
+    public void setHeroRole(String heroRole) {
+        this.heroRole = heroRole;
+    }
+
+    public double getAttackDamage() {
+        return attackDamage;
+    }
+
+    public void setAttackDamage(double attackDamage) {
+        this.attackDamage = attackDamage;
+    }
+
+    public double getArmor() {
+        return armor;
+    }
+
+    public void setArmor(double armor) {
+        this.armor = armor;
+    }
+
+    public String getAbilities() {
+        return abilities;
+    }
+
+    public void setAbilities(String abilities) {
+        this.abilities = abilities;
+    }
+
+
+
+
+}
